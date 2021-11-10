4c14ef7840e81ab34c31a9458254949dc9848469
alimadt <alima280300@gmail.com>
2021-11-10 13:38:25 +0600

    comment in views

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'MyBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
