f321c6afd000403ba1ff58e67960d6fdb7cd9452
alimadt <alima280300@gmail.com>
2021-11-11 14:47:54 +0600

    registration

---
+++ user/forms.py
@@ -0,0 +1,36 @@
+import email
+from django import forms
+from user.models import User
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(min_length=5, required=True, widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(min_length=5, required=True, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password_confirmation', 'first_name', 'last_name', 'image')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Пользователь с данным логином уже существует')
+        return username
+
+    def clean_email(self):
+        username = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с данной почтой уже существует')
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = data.get('password')
+        password_confirm = data.pop('password_confirmation')
+        if password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают')
+        return data
+
+    def save(self, commit=True):
+        user = User.objects.create_user(**self.cleaned_data)
+        return User
--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+class RegisterView(SuccessMessageMixin, CreateView):
+    model = User
+    template_name = 'user/registration.html'
+    form_class = RegistrationForm
+    success_url = reverse_lazy('home')
+    success_message = 'Вы успешно зарегистрированы на сайте'
